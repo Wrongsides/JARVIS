87c3e4e6fa8e8ec333b00d889eb9343219422783
Wrongsides <JohnByrne.1984@gmail.com>
2015-05-02 00:59:16 +0100

    fixed sickbeard.py location

--- sickrage/start.sh
+++ sickrage/start.sh
@@ -1,7 +1,4 @@
 #! /bin/sh
 umask 000
 
-exec /sbin/setuser jarvis python /opt/sickrage/SickBeard.py --datadir=/data
-
-# Did this because if Headphones restarts, it kills the container.
-tail -f /data/logs/SickRage.log
+exec /sbin/setuser jarvis python SickBeard.py --datadir=/data
