0d5496d37bc0662ae40a083a0de627441c40fa98
John Byrne <JohnByrne.1984@gmail.com>
2017-02-12 17:09:19 +0000

    redone sickrage

--- run-containers/sickrage.sh
+++ run-containers/sickrage.sh
@@ -1,5 +1,1 @@
-git clone git@github.com:timhaak/docker-sickrage.git
-cd docker-sickrage
-docker build -t sickrage .
-
-docker run --restart=always -d -h your_host_name -v /your_config_location:/config -v /your_videos_location:/data -p 8081:8081 sickrage
+docker run --restart=always -d -h JARVIS -v /mnt/share/config/sickrage:/config -v /mnt/share:/data -p 8081:8081 --name=sickrage wrongsides/sickrage
